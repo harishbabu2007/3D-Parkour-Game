a75a09ee20da8988fbdbf509899987dadc302e3f
harish <harishbabubalaji@gmail.com>
2022-03-07 20:17:00 +0530

    Added Gun Inventory
    Now we can pick up throw guns and dynamically add custom guns to the game

--- Assets/Scripts/Guns/Pistol.cs
+++ Assets/Scripts/Guns/Pistol.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShoot : MonoBehaviour
+public class Pistol : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator animator;
---
+++ Assets/Scripts/Player/GunManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct guns
+{
+  public string name;
+  public GameObject gunObj;
+  public GameObject ThrowObject;
+}
+
+public class GunManager : MonoBehaviour
+{
+  public List<guns> gunsList;
+  private string currentGunName = null;
+  public Transform gunThrowpoint, cam;
+  public float upwardThrowForce, forwardThrowForce;
+
+  private void Start()
+  {
+    foreach (var item in gunsList)
+    {
+      item.gunObj.SetActive(false);
+    }
+
+    cam = Camera.main.GetComponent<Transform>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyUp(KeyCode.Q) && currentGunName != null)
+    {
+      ThrowGun();
+    }
+  }
+
+  private void ThrowGun()
+  {
+    guns ThrowGunItem = gunsList.Find(item => item.name == currentGunName);
+    ThrowGunItem.gunObj.SetActive(false);
+
+    currentGunName = null;
+
+    GameObject ThrownGun = Instantiate(ThrowGunItem.ThrowObject, gunThrowpoint.position, Quaternion.identity);
+
+    ThrownGun.GetComponent<Rigidbody>().AddForce(cam.forward * forwardThrowForce, ForceMode.Impulse);
+    ThrownGun.GetComponent<Rigidbody>().AddForce(cam.forward * upwardThrowForce, ForceMode.Impulse);
+  }
+
+  private void OnControllerColliderHit(ControllerColliderHit hit)
+  {
+    foreach (var item in gunsList)
+    {
+      if (hit.gameObject.CompareTag(item.name))
+      {
+        currentGunName = item.name;
+        item.gunObj.SetActive(true);
+        Destroy(hit.gameObject);
+      }
+    }
+  }
+}
